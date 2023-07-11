225d953f0c26725023e10f35da7edccfc1b6ae6f
rawan-alwadiya <r.wadia21@gmail.com>
2023-07-11 06:33:32 +0100

    _

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bootcamp_starter/features/active_share/receive_view.dart';
 import 'package:bootcamp_starter/features/auth/login_view.dart';
 import 'package:bootcamp_starter/features/auth/register_view.dart';
 import 'package:bootcamp_starter/features/home/home_view.dart';
+import 'package:bootcamp_starter/features/home/qr_scan.dart';
 import 'package:bootcamp_starter/features/main_app/main_app_view.dart';
 import 'package:bootcamp_starter/features/onbording/onbording_view.dart';
 import 'package:bootcamp_starter/features/profile/profile_view.dart';
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
         MainAppView.id: (context) => const MainAppView(),
         ProfileView.id: (context) => const ProfileView(),
         ReceiveView.id: (context) => const ReceiveView(),
+        ScanCode.id: (context) =>  ScanCode(),
       },
     );
   }
